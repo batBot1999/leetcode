64d4c3c752c3d957bb7c40c4c2ec6074f0450ba9
Ashenone <2657518095@qq.com>
2023-12-13 22:49:35 +0800

    lc test

--- 二分查找/69. E- x 的平方根.js
+++ 二分查找/69. E- x 的平方根.js
@@ -2,20 +2,6 @@
  * @param {number} x
  * @return {number}
  */
-//  const mySqrt = function(x) {
-//   if (x < 2) return x
-//   let left = 1, mid, right = Math.floor(x / 2);
-//   while (left <= right) {
-//      mid = Math.floor(left + (right - left) / 2)
-//      if (mid * mid === x) return mid
-//      if (mid * mid < x) {
-//          left = mid + 1
-//      }else {
-//          right = mid - 1
-//      }
-//   }
-//   return right
-// }
 
  var mySqrt = function(x) {
   if (x < 2) return x;
